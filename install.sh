fc02d4b34caf983fa2f5c7d797494e2600aed326
Tom Sullivan <tom@msbit.com.au>
2018-11-21 11:29:49 +1100

    Clean up package list creation

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@
 
 DOWNLOAD_BASE=$(realpath ${DOWNLOAD_PATH})
 
-PACKAGES="${DOWNLOAD_BASE}/linux-headers-${KVER}-${KPATCH}_${KVER}-${KPATCH}.${PVER}_all.deb ${DOWNLOAD_BASE}/linux-headers-${KVER}-${KPATCH}-${KVAR}_${KVER}-${KPATCH}.${PVER}_${ARCH}.deb ${DOWNLOAD_BASE}/linux-image-${KVER}-${KPATCH}-${KVAR}_${KVER}-${KPATCH}.${PVER}_${ARCH}.deb ${DOWNLOAD_BASE}/linux-signed-image-${KVER}-${KPATCH}-${KVAR}_${KVER}-${KPATCH}.${PVER}_${ARCH}.deb"
+PACKAGES="${DOWNLOAD_BASE}/linux-headers-${KVER}-${KPATCH}_${KVER}-${KPATCH}.${PVER}_all.deb"
+PACKAGES="${PACKAGES} ${DOWNLOAD_BASE}/linux-headers-${KVER}-${KPATCH}-${KVAR}_${KVER}-${KPATCH}.${PVER}_${ARCH}.deb"
+PACKAGES="${PACKAGES} ${DOWNLOAD_BASE}/linux-image-${KVER}-${KPATCH}-${KVAR}_${KVER}-${KPATCH}.${PVER}_${ARCH}.deb"
+PACKAGES="${PACKAGES} ${DOWNLOAD_BASE}/linux-signed-image-${KVER}-${KPATCH}-${KVAR}_${KVER}-${KPATCH}.${PVER}_${ARCH}.deb"
 
 if [ "${KVAR}" == "generic" ]
 then
